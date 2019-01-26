3e18cad4dbe5c34957414a8df399618a9aba7492
Sergio R Pérez <37162018+SergioOwls@users.noreply.github.com>
2019-01-26 14:05:38 -0400

    added Hazards
    	1. Logs move to the right.
    	2. LillyPads are fixed in position.
    
    	- Has Infinite Loop
    	- Varies with floor tiles
    		. Currently only implemented with water.

--- src/Game/World/WorldManager.java
+++ src/Game/World/WorldManager.java
@@ -16,8 +16,9 @@ public class WorldManager {
     ArrayList<BaseArea> AreasAvailables;//Lake, empty and grass area
     ArrayList<StaticBase> StaticEntitiesAvailables;//trees, lillies, logs
     
-    ArrayList<BaseArea> SpawnedAreas;//Areas currently on world
-
+    ArrayList<BaseArea> SpawnedAreas;		//Areas currently on world
+    ArrayList<StaticBase> SpawnedHazards;	//Hazards currently on world.
+    
     Handler handler;
 
     Player player;
@@ -37,17 +38,18 @@ public class WorldManager {
         AreasAvailables.add(new WaterArea(handler, 0));
         AreasAvailables.add(new EmptyArea(handler, 0));
 
-        SpawnedAreas = new ArrayList<>();
-
-        StaticEntitiesAvailables.add(new LillyPad(handler));
-        StaticEntitiesAvailables.add(new Log(handler));
+        StaticEntitiesAvailables.add(new LillyPad(handler, 0, 0));
+        StaticEntitiesAvailables.add(new Log(handler, 0, 0));
         StaticEntitiesAvailables.add(new Tree(handler));
 
+        SpawnedAreas = new ArrayList<>();
+        SpawnedHazards = new ArrayList<>();
+        
         player = new Player(handler);
 
         gridWidth = handler.getWidth()/64;
         gridHeight = handler.getHeight()/64;
-        movementSpeed = 1;
+        movementSpeed = 2;
         
         //Spawn Areas in Map (2 extra areas spawned off screen)
         for(int i=0; i<gridHeight+2; i++) {
@@ -69,9 +71,11 @@ public class WorldManager {
         for(BaseArea area : SpawnedAreas) {
             area.tick();
         }
+        
     	for(int i=0; i<SpawnedAreas.size(); i++) {
     		SpawnedAreas.get(i).setYPosition(SpawnedAreas.get(i).getYPosition()+movementSpeed);
-     		  	//Check if Area passed the screen
+    		
+     		  	//Check if Area (thus a hazard as well) passed the screen.
     			if(SpawnedAreas.get(i).getYPosition() > handler.getHeight()) {
     				//Replace with a new random area and position it on top
      			   SpawnedAreas.set(i, randomArea(-2*64));
@@ -79,7 +83,23 @@ public class WorldManager {
     			if(SpawnedAreas.get(i).getYPosition() < player.getY() && player.getY()-SpawnedAreas.get(i).getYPosition()<3) {
     				player.setY(SpawnedAreas.get(i).getYPosition());
     			}
+    			
+    			if (!(SpawnedHazards.size() <= i)) {
+    				SpawnedHazards.get(i).setY(SpawnedHazards.get(i).getY() + movementSpeed);
+    				
+    				if (SpawnedHazards.get(i) instanceof Log){
+    				SpawnedHazards.get(i).setX(SpawnedHazards.get(i).getX() + (movementSpeed / 2));
+    				}
+    				
+    				// if hazard has passed the screen height, then remove this hazard.
+    				if(SpawnedHazards.get(i).getY() > handler.getHeight()) {
+    					SpawnedHazards.remove(i);
+    				}
+    			}
         }
+    	
+    	
+    
         player.tick();
         //make player move the same as the areas
         player.setY(player.getY()+movementSpeed);
@@ -89,7 +109,12 @@ public class WorldManager {
        for(BaseArea area : SpawnedAreas) {
     	   area.render(g);
        }
-    	player.render(g);
+    	
+       for (StaticBase hazards : SpawnedHazards) {
+    		hazards.render(g);
+       }
+    	
+       player.render(g);
     }
     
     //Returns a random area
@@ -101,11 +126,33 @@ public class WorldManager {
     	}
     	else if(randomArea instanceof WaterArea) {
     		randomArea = new WaterArea(handler, yPosition);
+    		SpawnHazard(yPosition);
     	}
     	else {
     		randomArea = new EmptyArea(handler, yPosition);
     	}
     	return randomArea;
     }
+
+    // TODO:
+    //	1. Add Tree choice implementation
+    // 	2. Deny two Lillypads from spawning one after the other.
+	//	3. Add multiple Logs with same speed in the same AreaRow.
+	private void SpawnHazard(int yPosition) {
+		Random rand = new Random();
+		int randInt;
+		
+		// Chooses between Log or Lillypad
+		if (rand.nextBoolean()) {
+			randInt = 64 * rand.nextInt(4);
+    		SpawnedHazards.add(new Log(handler, randInt, yPosition));
+		}
+		else {
+			randInt = 64 * rand.nextInt(9);
+			SpawnedHazards.add(new LillyPad(handler, randInt, yPosition));
+		}
+	}
+    
+    
     
 }
